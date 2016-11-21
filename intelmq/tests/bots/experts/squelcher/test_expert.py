ef26e0a764d22e984f2860b6f4e1676e54f9c5a5
Sebastian Wagner <sebix@sebix.at>
2016-11-21 19:01:48 +0100

    TST: squelcher: shorten and truncate often
    Signed-off-by: Sebastian Wagner <sebix@sebix.at>

--- intelmq/tests/bots/experts/squelcher/test_expert.py
+++ intelmq/tests/bots/experts/squelcher/test_expert.py
@@ -14,8 +14,10 @@ from intelmq.bots.experts.squelcher.expert import SquelcherExpertBot
 INSERT_QUERY = '''
 INSERT INTO {table}(
     "classification.identifier", "classification.type", notify, "source.asn",
-    "source.ip", "time.source"
-) VALUES (%s, %s, %s, %s, %s, LOCALTIMESTAMP + INTERVAL %s second);
+    "source.ip", "time.source", "sent_at"
+) VALUES (%s, %s, %s, %s, %s,
+    LOCALTIMESTAMP + INTERVAL %s second,
+    LOCALTIMESTAMP + INTERVAL %s second);
 '''
 INPUT1 = {"__type": "Event",
           "classification.identifier": "zeus",
@@ -36,8 +38,6 @@ INPUT3 = INPUT1.copy()
 INPUT3["classification.identifier"] = "https"
 INPUT3["classification.type"] = "vulnerable service"
 INPUT3["source.ip"] = "192.0.2.4"
-OUTPUT3 = INPUT3.copy()
-OUTPUT3["notify"] = True
 
 INPUT4 = INPUT3.copy()
 INPUT4["classification.identifier"] = "openresolver"
@@ -117,106 +117,134 @@ class TestSquelcherExpertBot(test.BotTestCase, unittest.TestCase):
         cls.cur.execute("TRUNCATE TABLE {}".format(cls.sysconfig['table']))
         global INSERT_QUERY
         INSERT_QUERY = INSERT_QUERY.format(table=cls.sysconfig['table'])
+        cls.truncate(cls)
+
+    def truncate(self):
+        self.cur.execute("TRUNCATE TABLE {}".format(self.sysconfig['table']))
 
     def test_ttl_1(self):
-        query_data = ('zeus', 'botnet drone', True, 0, '192.0.2.1', '0')
+        "event exists in db -> squelch"
+        query_data = ('zeus', 'botnet drone', True, 0, '192.0.2.1', '0', '0')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT1
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 604800 for', levelname='DEBUG')
         self.assertMessageEqual(0, INPUT1)
 
     def test_ttl_2(self):
+        "event in db is too old -> notify"
         query_data = ('https', 'vulnerable service', True, 0, '192.0.2.1',
-                      '- 01:45')
+                      '- 01:45', '- 01:45')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT2
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 3600 for', levelname='DEBUG')
         self.assertMessageEqual(0, OUTPUT2)
 
-    def test_ttl_2h_notify(self):
-        query_data = ('https', 'vulnerable service', True, 0, '192.0.2.4',
-                      '- 02:45')
-        self.cur.execute(INSERT_QUERY, query_data)
-        self.input_message = INPUT3
-        self.run_bot()
-        self.assertMessageEqual(0, OUTPUT3)
-
     def test_ttl_2h_squelch(self):
+        "event is in db -> squelch"
         query_data = ('https', 'vulnerable service', True, 0, '192.0.2.4',
-                      '- 01:45')
+                      '- 01:45', '- 01:45')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT3
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 7200 for', levelname='DEBUG')
         self.assertMessageEqual(0, INPUT3)
 
     def test_network_match(self):
+        """event is in db without notify -> notify
+        find ttl based on network test"""
         query_data = ('openresolver', 'vulnerable service', False, 0,
-                      '198.51.100.5', '- 20:00')
+                      '198.51.100.5', '- 20:00', '- 20:00')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT5
         self.run_bot()
-        self.assertMessageEqual(0, INPUT5)
-
-    def test_network_match2(self):
-        query_data = ('openresolver', 'vulnerable service', False, 0,
-                      '198.51.100.5', '- 25:00')
-        self.cur.execute(INSERT_QUERY, query_data)
-        self.input_message = INPUT5
-        self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 115200 for', levelname='DEBUG')
         self.assertMessageEqual(0, INPUT5)
 
     def test_network_match3(self):
+        """event is in db -> squelch
+        find ttl based on network test"""
         query_data = ('openresolver', 'vulnerable service', True, 0,
-                      '198.51.100.5', '- 25:00')
+                      '198.51.100.5', '- 25:00', '- 25:00')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT5
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 115200 for', levelname='DEBUG')
         self.assertMessageEqual(0, OUTPUT5)
 
     def test_address_match1(self):
+        "event in db is too old -> notify"
         query_data = ('openresolver', 'vulnerable service', True, 0,
-                      '198.51.100.45', '- 25:00')
+                      '198.51.100.45', '- 25:00', '- 25:00')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT6
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 86400 for', levelname='DEBUG')
         self.assertMessageEqual(0, INPUT6)
 
     def test_address_match2(self):
+        "event is in db -> squelch"
         query_data = ('openresolver', 'vulnerable service', True, 0,
-                      '198.51.100.45', '- 20:00')
+                      '198.51.100.45', '- 20:00', '- 20:00')
         self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT6
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 86400 for', levelname='DEBUG')
         self.assertMessageEqual(0, OUTPUT6)
 
     def test_ttl_other_ident(self):
+        "other event in db -> notify"
+        query_data = ('https', 'vulnerable service', True, 0, '198.51.100.5',
+                      '- 01:45', '- 01:45')
+        self.cur.execute(INSERT_QUERY, query_data)
         self.input_message = INPUT4
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL 7200 for', levelname='DEBUG')
         self.assertMessageEqual(0, INPUT4)
 
     def test_domain(self):
+        "only domain -> notify"
         self.input_message = INPUT7
         self.run_bot()
+        self.truncate()
+        self.assertNotRegexpMatchesLog("Found TTL")
         self.assertMessageEqual(0, OUTPUT7)
 
     def test_missing_asn(self):
+        "no asn -> notify"
         self.input_message = INPUT8
         self.run_bot()
+        self.truncate()
+        self.assertNotRegexpMatchesLog("Found TTL")
         self.assertMessageEqual(0, OUTPUT8)
 
     def test_infinite(self):
+        "never notify with ttl -1"
         self.input_message = INPUT_INFINITE
         self.run_bot()
+        self.truncate()
+        self.assertLogMatches('Found TTL -1 for', levelname='DEBUG')
         self.assertMessageEqual(0, OUTPUT_INFINITE)
 
     def test_iprange(self):
+        "test if mechanism checking IP ranges"
         self.input_message = INPUT_RANGE
         self.run_bot()
+        self.truncate()
         self.assertLogMatches('Found TTL 72643 for', levelname='DEBUG')
 
     @classmethod
     def tearDownClass(cls):
-        cls.cur.execute("TRUNCATE TABLE {}".format(cls.sysconfig['table']))
+        cls.truncate(cls)
         cls.cur.close()
         cls.con.close()
 
